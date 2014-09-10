bd20636505c3a522ca4c8678149ba03452990059
jp <wog.jplacsinto@gmail.com>
2014-09-10 14:52:54 +0800

    customized a bit

---
+++ app/main.js
@@ -0,0 +1,41 @@
+(function() {
+
+  function hideLoader() {
+    document.getElementById('loader').style.display = 'none';
+  }
+  function showLoader() {
+    document.getElementById('loader').style.display = 'block';
+  }
+
+  window.onload = function() {
+    showLoader();
+    var socket = io.connect();
+    socket.on('connect', function() {
+      hideLoader();
+      var term = new Terminal({
+        cols: 80,
+        rows: 24
+      });
+
+      term.on('data', function(data) {
+        socket.emit('data', data);
+      });
+
+      term.on('title', function(title) {
+        document.title = title;
+      });
+
+      term.open(document.body);
+
+      term.write('welcome to whim terminal!\n');
+
+      socket.on('data', function(data) {
+        term.write(data);
+      });
+
+      socket.on('disconnect', function() {
+        term.destroy();
+      });
+    });
+  };
+}).call(this);
